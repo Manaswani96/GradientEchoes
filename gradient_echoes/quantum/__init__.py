514e9363f97a53a8157589984a886045d7a4e863
Mahi.K <katikelamanaswani9969p@gmail.com>
2025-09-30 23:52:36 +0530

    api: export parameter_shift_grad, QAOA, and QSPSA from quantum module

--- gradient_echoes/quantum/__init__.py
+++ gradient_echoes/quantum/__init__.py
@@ -1,4 +1,7 @@
 from .qng import QNG
 from .vqe import VQE
+from .parameter_shift import parameter_shift_grad
+from .qaoa import QAOA
+from .spsa_q import QSPSA
 
-__all__ = ["QNG", "VQE"]
+__all__ = ["QNG", "VQE", "parameter_shift_grad", "QAOA", "QSPSA"]
