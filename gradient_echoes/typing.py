1ebcff7d5882e24554ec73cc4d53f3084e4a3e4a
Manaswani96 <katikelamanaswani9969p@gmail.com>
2025-09-30 22:12:25 +0530

    Structuring

--- gradient_echoes/typing.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-from typing import Callable, Protocol, Any, Dict, Tuple, Iterable, Optional, Union
-
-Scalar = float
-Vector = Union[float, Any]  # Any: numpy.ndarray or torch.Tensor later
-PRNGKey = Optional[int]
-
-class StepFn(Protocol):
-    def __call__(self, t: int) -> float: ...
