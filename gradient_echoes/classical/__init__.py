514703996831c420d92df55f4efdcf8eb25f674e
Mahi.K <katikelamanaswani9969p@gmail.com>
2025-09-30 23:52:05 +0530

    api: export RMSProp, AdaGrad, AMSGrad, Nelder–Mead, SPSA in classical module

--- gradient_echoes/classical/__init__.py
+++ gradient_echoes/classical/__init__.py
@@ -1,5 +1,10 @@
 from .sgd import SGD
 from .adam import Adam
 from .lbfgs import LBFGS
+from .rmsprop import RMSProp
+from .adagrad import AdaGrad
+from .amsgrad import AMSGrad
+from .nelder_mead import NelderMead
+from .spsa import SPSA
 
-__all__ = ["SGD", "Adam", "LBFGS"]
+__all__ = ["SGD", "Adam", "LBFGS", "RMSProp", "AdaGrad", "AMSGrad", "NelderMead", "SPSA"]
